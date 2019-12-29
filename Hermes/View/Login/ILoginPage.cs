4d01fc1d69578e7750fe4c382d7becc546b60e05
Tasos Tsantoulis <tsdi69@hotmail.com>
2019-12-29 19:12:29 +0200

    Login transfer from WPF

---
+++ Hermes/View/Login/ILoginPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.View
+{
+    public interface ILoginpage
+    {
+        string LabelUsername { set; get; }
+        string LabelPassword { set; get; }
+
+        string ErrorDialog { set; }
+    }
+}
---
+++ Hermes/View/Login/LoginPresenter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermes.Model;
+using Hermes.Model.Models;
+using Hermes.View;
+
+namespace Hermes.View
+{
+    class LoginPresenter
+    {
+        private ILoginpage _view;
+        private LoginRepository _login;
+
+        public LoginPresenter(ILoginpage view)
+        {
+            _view = view;
+            _login = new LoginRepository();
+        }
+
+        public void UserExist()
+        {
+            //TODO: change this to private 
+            string _username=_view.LabelUsername;
+            string _password=_view.LabelPassword;
+
+            
+            string result = _login.UserExist(_username,_password);
+            //Convert strint to int
+            int ResultInt = Int32.Parse(result);
+         
+            if (ResultInt == 1)
+            {
+                User Loggedin = _login.GetUserData(_username);
+                //TODO : PASS IT TO THE NEXT PRESENTER
+            }
+            else
+            {
+                _view.ErrorDialog = "Username and password does not match";
+            }
+        }
+    }
+            
+
+ }
+
+    
+
