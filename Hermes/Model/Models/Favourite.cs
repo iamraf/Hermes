35bc88ea1a0754451f0482dbd8b13b41ae5a2464
ChristosTanailidis <32941481+ChristosTanailidis@users.noreply.github.com>
2020-01-01 19:36:02 +0200

    Add item on user favourites

--- Hermes/Model/Models/Favourite.cs
+++ Hermes/Model/Models/Favourite.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Hermes.Model.Models
 {
     class Favourite
     {
+        private MySqlConnection _connection;
         public int ListingId { get; }
         public int UserId { get; }
 
@@ -15,6 +17,66 @@ namespace Hermes.Model.Models
         {
             ListingId = listingId;
             UserId = userId;
+
+            string connectionString = "SERVER=remotemysql.com;DATABASE=4G6ccccjnC;UID=4G6ccccjnC;PASSWORD=l0YkuReQwW;";
+            _connection = new MySqlConnection(connectionString);
+
+            if (AddItemOnFavourites() == true)
+                //show done
+                Console.WriteLine("Item added on fav");
+            else
+                //not done
+                Console.WriteLine("Item could not be added on fav");
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                _connection.Open();
+
+                return true;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+        }
+
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                _connection.Close();
+
+                return true;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+        }
+
+        private bool AddItemOnFavourites()
+        {
+            if(this.OpenConnection() == true)
+            {
+                string query = "INSERT INTO User_Favorites (listingID, userID) VALUE ("+ListingId+", "+UserId+")";
+               
+                MySqlCommand cmd = new MySqlCommand(query, _connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read()) { }
+
+                dataReader.Close();
+
+                this.CloseConnection();
+
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
